7e2c1643242707729b13e09745ede414651c4736
Vanikiller <divinethankgod227@gmail.com>
2024-10-23 00:54:06 +0100

    Update main.js
    Signed-off-by: Vanikiller <divinethankgod227@gmail.com>

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = "50936698203"
+let phoneNumber = "2347070492951"
 let owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
